47d0bbcc9f9665f59973696d49ff3ec5f0e6968d
shreyan-oneness <shreyan.dey@onenesstechs.in>
2022-11-16 20:00:02 +0530

    updated by shreyan on nov 16

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -9,7 +9,7 @@ class OrderController extends Controller
 {
     public function index(Request $request)
     {
-    $orders = Order::where('user_id', auth()->guard('web')->user()->id)->with('orderProducts')->get();
+    $orders = Order::where('user_id', auth()->guard('web')->user()->id)->with('orderProducts')->orderBy('id','DESC')->get();
     //dd($orders);
     return view('front.order.index', compact('orders'));
     }
