86b18705f871bd8c48a9afed79e383acf4a13977
sourabpaulcnp <sourab.paul@capitalnumbers.com>
2022-11-28 16:14:42 +0530

    cart page design

--- resources/views/front/cart/index.blade.php
+++ resources/views/front/cart/index.blade.php
@@ -3,31 +3,34 @@
 @section('title', 'Cart')
 
 @section('section')
+
 <style>
-.cart-item{
-    width: calc(100% / 4) !important;
-}
-.cart-item.item-qty .qty-box a {
-    width: 20px;
-    height: 20px;
-    border: none;
-    background: #101010;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    padding: 3px;
-}
-.cart-item.item-qty .qty-box a:hover {
-    background: #c10909;
-}
-.cart-item.item-qty .qty-box a svg {
-    width: 14px;
-    height: 14px;
-}
+    .cart-item{
+        width: calc(100% / 4) !important;
+    }
+    .cart-item.item-qty .qty-box a {
+        width: 20px;
+        height: 20px;
+        border: none;
+        background: #101010;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        padding: 3px;
+    }
+    .cart-item.item-qty .qty-box a:hover {
+        background: #c10909;
+    }
+    .cart-item.item-qty .qty-box a svg {
+        width: 14px;
+        height: 14px;
+    }
 </style>
+
+
 @if(count($data) > 0)
 <section class="cart-header mt-5 mb-3 mb-sm-5">
     <div class="container">
@@ -48,125 +51,111 @@
         <div class="alert alert-danger">{{Session::get('failure')}}</div>
         @endif --}}
 
-        <div class="cart-holder">
-            {{-- <div class="cart-row cart-row--header">
-                <div class="cart-item item-thumb">Image</div>
-                <div class="cart-item item-title">Name</div>
-                <div class="cart-item item-attr">Author</div>
-                <div class="cart-item item-color">Contents</div>
-                <div class="cart-item item-price">Price</div>
-                <div class="cart-item item-remove">Action</div>
-            </div> --}}
-
-            @php
-                $subTotal = $grandTotal = $couponCodeDiscount = $shippingCharges = $taxPercent = 0;
-            @endphp
-
-            @foreach($data as $cartKey => $cartValue)
-            <div class="cart-row">
-                <div class="cart-item item-thumb">
-                    <figure>
-                       <img style="width: 100px;height: 100px;" src="{{asset($cartValue->course_image)}}">
-                    </figure>
+        <div class="row justify-content-between">
+            <div class="col-lg-7">
+                <div class="cart__item">
+                    {{-- <div class="cart-row cart-row--header">
+                        <div class="cart-item item-thumb">Image</div>
+                        <div class="cart-item item-title">Name</div>
+                        <div class="cart-item item-attr">Author</div>
+                        <div class="cart-item item-color">Contents</div>
+                        <div class="cart-item item-price">Price</div>
+                        <div class="cart-item item-remove">Action</div>
+                    </div> --}}
+                    @php
+                        $subTotal = $grandTotal = $couponCodeDiscount = $shippingCharges = $taxPercent = 0;
+                    @endphp
+                    @foreach($data as $cartKey => $cartValue)
+                    <div class="d-flex">
+                        <div class="item__image">
+                            {{--<img src="{{asset($cartValue->course_image)}}">--}}
+                            <img src="https://cdn.pixabay.com/photo/2018/02/27/10/49/training-3185170__480.jpg" alt="">
+                        </div>
+                        <div class="item__content">
+                            <div class="item-title">
+                                @if($cartValue->purchase_type != 'subscription')
+                                    <h5>{{$cartValue->course_name}}</h5>
+                                @else
+                                    <h5>{{$cartValue->course_name}} Subscription</h5>
+                                @endif
+                            </div>
+                            <div class="item-author text-muted">
+                                @if($cartValue->purchase_type != 'subscription')
+                                    @if($cartValue->purchase_type != 'deal')
+                                        <small>By {{$cartValue->author_name}}</small>
+                                    {{-- @else
+                                        <h6>-- Deal --</h6> --}}
+                                    @endif
+                                {{-- @else
+                                    <h6>-- Subscription --</h6> --}}
+                                @endif
+                                <small>Suvajit Bardhan</small>
+                            </div>
+                            <div class="item-author text-muted">
+                                @if($cartValue->purchase_type == 'course')
+                                <p><li>{{totalLessonsAndTopics($cartValue->course_id)->lesson_count}} Lessons</li></p>
+                                <p><li>{{totalLessonsAndTopics($cartValue->course_id)->topic_count}} Topics</li></p>
+                                {{-- @else
+                                    @if($cartValue->purchase_type != 'deal')    
+                                        <h6>-- Subscription --</h6>
+                                    @else
+                                        <h6>-- Deal --</h6>
+                                    @endif --}}
+                                @endif
+                            </div>
+                            <div class="item__btn">
+                                <a href="javascript:void(0)" data-link="{{route('front.cart.delete', $cartValue->id)}}" class="delete_cart_item"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg><!--<span>Remove</span>--></a>                                
+                            </div>
+                        </div>
+                        <div class="item__price">
+                            <h5>${{$cartValue->price * $cartValue->qty}} (${{$cartValue->price}} x {{$cartValue->qty}})</h5>
+                        </div>
+                    </div>
+                    @php
+                        // subtotal calculation
+                        $subTotal += (int) $cartValue->price * $cartValue->qty;
+                        // grand total calculation
+                        $grandTotalWithoutCoupon = $subTotal;
+                        $grandTotal = ($subTotal + $shippingCharges);
+                    @endphp
+                    @endforeach
                 </div>
-                <div class="cart-item">
-                    <div class="item-title h5">
-                        @if($cartValue->purchase_type != 'subscription')
-                            <h6>{{$cartValue->course_name}}</h6>
-                        @else
-                            <h6>{{$cartValue->course_name}} Subscription</h6>
-                        @endif
+            </div>
+            <div class="col-lg-4">
+                <div class="cart-summary">
+                    <div class="cart-total">
+                        <div class="cart-total-label">
+                            Subtotal
+                        </div>
+                        <div class="cart-total-value">
+                            $<span id="subTotalAmount">{{$subTotal}}</span>
+                        </div>
                     </div>
-                    <div class="item-author text-muted">
-                        @if($cartValue->purchase_type != 'subscription')
-                            @if($cartValue->purchase_type != 'deal')
-                                <small>By {{$cartValue->author_name}}</small>
-                            {{-- @else
-                                <h6>-- Deal --</h6> --}}
-                            @endif
-                        {{-- @else
-                            <h6>-- Subscription --</h6> --}}
-                        @endif
+                    <div class="cart-total">
+                        <div class="cart-total-label">
+                            Tax
+                        </div>
+                        <div class="cart-total-value">
+                            ${{$shippingCharges}}
+                        </div>
                     </div>
-                    <div class="item-author text-muted">
-                        @if($cartValue->purchase_type == 'course')
-                            <p><li>{{totalLessonsAndTopics($cartValue->course_id)->lesson_count}} Lessons</li></p>
-                            <p><li>{{totalLessonsAndTopics($cartValue->course_id)->topic_count}} Topics</li></p>
-                        {{-- @else
-                            @if($cartValue->purchase_type != 'deal')    
-                                <h6>-- Subscription --</h6>
-                            @else
-                                <h6>-- Deal --</h6>
-                            @endif --}}
-                        @endif
-                    </div>
-                </div>
+                    {{-- <div class="cart-total">
+                        <div class="cart-total-label">
+                            Tax and Others - <strong>{{$taxPercent}}%</strong><br/>
+                            <small>(Inclusive of all taxes)</small>
+                        </div>
+                        <div class="cart-total-value"></div>
+                    </div> --}}
                 
-                <div class="cart-item item-price">
-                    <h4>${{$cartValue->price * $cartValue->qty}} (${{$cartValue->price}} x {{$cartValue->qty}})</h4>
-                </div>
-                <div class="cart-item item-remove">
-                    <a href="javascript:void(0)" data-link="{{route('front.cart.delete', $cartValue->id)}}" class="delete_cart_item"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg><!--<span>Remove</span>--></a>
-                </div>
-            </div>
-
-            @php
-                // subtotal calculation
-                $subTotal += (int) $cartValue->price * $cartValue->qty;
-
-               
-                // grand total calculation
-                $grandTotalWithoutCoupon = $subTotal;
-                $grandTotal = ($subTotal + $shippingCharges);
-            @endphp
-
-            @endforeach
-        </div>
-    </div>
-    <div class="container mt-3 mt-sm-5">
-        <div class="cart-summary">
-            <div class="row justify-content-between flex-row-reverse">
-                <div class="col-md-6 col-lg-5">
-                    <div class="w-100">
-                        <div class="cart-total">
-                            <div class="cart-total-label">
-                                Subtotal
-                            </div>
-                            <div class="cart-total-value">
-                                $<span id="subTotalAmount">{{$subTotal}}</span>
-                            </div>
+                    <div class="cart-total">
+                        <div class="cart-total-label">
+                            Total
                         </div>
-                        <div class="cart-total">
-                            <div class="cart-total-label">
-                                Tax
-                            </div>
-                            <div class="cart-total-value">
-                                ${{$shippingCharges}}
-                            </div>
-                        </div>
-                        {{-- <div class="cart-total">
-                            <div class="cart-total-label">
-                                Tax and Others - <strong>{{$taxPercent}}%</strong><br/>
-                                <small>(Inclusive of all taxes)</small>
-                            </div>
-                            <div class="cart-total-value"></div>
-                        </div> --}}
-                       
-                        <div class="cart-total">
-                            <div class="cart-total-label">
-                                Total
-                            </div>
-                            <div class="cart-total-value">
-                                <input type="hidden" value="{{$grandTotalWithoutCoupon}}" name="grandTotalWithoutCoupon">
-                                $<span id="displayGrandTotal">{{$grandTotal}}</span>
-                            </div>
+                        <div class="cart-total-value">
+                            <input type="hidden" value="{{$grandTotalWithoutCoupon}}" name="grandTotalWithoutCoupon">
+                            $<span id="displayGrandTotal">{{$grandTotal}}</span>
                         </div>
                     </div>
-                </div>
-               
-            </div>
-            <div class="row justify-content-between">
-                <div class="col-sm-12 text-end mt-4">
                     <form action="{{route('front.checkout.index')}}" method="GET">
                         <button type="submit" class="btn checkout-btn button">Proceed to checkout</button>
                     </form>
